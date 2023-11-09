6509d64f5e154a9ee44f98d231a60e8ff3e4214e
José Caldas <jrcaldas@yahoo.com.br>
2023-11-09 09:50:51 -0300

    add axios

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -14,6 +14,7 @@
 </template>
 
 <script>
+import { api } from '@/services.js'
 export default {
   name: 'ProdutosLista',
   data() {
@@ -23,10 +24,13 @@ export default {
   },
   methods: {
     getprodutos() {
-      fetch('http://localhost:3000/produto')
-        .then((response) => response.json())
+      api
+        .get('/produto')
         .then((response) => {
-          this.produtos = response
+          this.produtos = response.data
+        })
+        .catch((error) => {
+          console.log(error)
         })
     },
   },
