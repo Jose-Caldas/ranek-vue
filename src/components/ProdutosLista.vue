eb80adeab3679fa60aacc4f01578d73be1d4e0f6
José Caldas <jrcaldas@yahoo.com.br>
2023-11-08 21:36:27 -0300

    add fetch product

---
+++ src/components/ProdutosLista.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="produtos_container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img
+        v-if="produto.fotos"
+        v-bind:src="produto.fotos[0].src"
+        :alt="produto.fotos[0].titulo"
+      />
+      <p class="preco">{{ produto.preco }}</p>
+      <h2 class="title">{{ produto.nome }}</h2>
+      <p>{{ produto.descricao }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ProdutosLista',
+  data() {
+    return {
+      produtos: null,
+    }
+  },
+  methods: {
+    getprodutos() {
+      fetch('http://localhost:3000/produto')
+        .then((response) => response.json())
+        .then((response) => {
+          this.produtos = response
+        })
+    },
+  },
+  created() {
+    this.getprodutos()
+  },
+}
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <template>
-  <div></div>
+  <section>
+    <h1>👍Compre ou Venda👎</h1>
+    <ProdutosBusca />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
+import ProdutosBusca from '@/components/ProdutosBusca.vue'
+import ProdutosLista from '@/components/ProdutosLista.vue'
 export default {
   name: 'HomeView',
+  components: {
+    ProdutosBusca,
+    ProdutosLista,
+  },
 }
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  font-size: 2rem;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
