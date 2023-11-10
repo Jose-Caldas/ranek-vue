5874d8a3f11c5312014a7521bd3f2e386e139f81
José Caldas <jrcaldas@yahoo.com.br>
2023-11-10 15:56:43 -0300

    add transition to product list

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import PaginaCarregandoVue from './components/PaginaCarregando.vue'
 
 Vue.config.productionTip = false
+Vue.component('PaginaCarregando', PaginaCarregandoVue)
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
