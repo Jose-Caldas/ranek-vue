17303c77a49032d4facc16d5ef10f2954fea8cb6
José Caldas <jrcaldas@yahoo.com.br>
2023-11-13 21:03:40 -0300

    start form to create login

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -1,13 +1,45 @@
 <template>
-  <div>
-    <p>Criar Login</p>
-  </div>
+  <section>
+    <h2>Crie sua conta</h2>
+    <transition mode="out-in">
+      <button class="btn criar" @click="criar = true" v-if="!criar">
+        Criar Conta
+      </button>
+      <UsuarioForm v-else>
+        <button class="btn btn-form">Criar Usuário</button>
+      </UsuarioForm>
+    </transition>
+  </section>
 </template>
 
 <script>
+import UsuarioForm from './UsuarioForm.vue'
+
 export default {
   name: 'LoginCriar',
+  components: { UsuarioForm },
+  data() {
+    return {
+      criar: false,
+    }
+  },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+h2 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin: 0 auto;
+}
+
+.btn-form {
+  max-width: 100%;
+}
+</style>
